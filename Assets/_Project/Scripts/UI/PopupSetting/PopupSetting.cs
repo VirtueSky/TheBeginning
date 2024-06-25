1e236f8ecd89df996f543ccf1490ca1d8b068c00
DucNV <ducnv@gameestudio.com>
2024-06-25 16:40:02 +0700

    -update

--- Assets/_Project/Scripts/UI/PopupSetting/PopupSetting.cs
+++ Assets/_Project/Scripts/UI/PopupSetting/PopupSetting.cs
@@ -1,5 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using VirtueSky.Events;
+#if ADS_ADMOB
+using GoogleMobileAds.Ump.Api;
+#endif
 
 public class PopupSetting : UIPopup
 {
-    
+    [SerializeField] private Button btnRestorePurchase;
+    [SerializeField] private Button btnShowPrivacyConsent;
+    [SerializeField] private EventNoParam restorePurchaseEvent;
+    [SerializeField] private EventNoParam callShowAgainGDPREvent;
+
+    protected override void OnBeforeShow()
+    {
+        base.OnBeforeShow();
+        btnRestorePurchase.onClick.AddListener(OnClickRestorePurchase);
+        btnShowPrivacyConsent.onClick.AddListener(OnClickShowPrivacyConsent);
+        SetupButtonDefault();
+#if UNITY_IOS
+        btnRestorePurchase.gameObject.SetActive(true);
+#endif
+#if ADS_ADMOB
+        btnShowPrivacyConsent.gameObject.SetActive(ConsentInformation.PrivacyOptionsRequirementStatus ==
+                                                   PrivacyOptionsRequirementStatus.Required);
+#endif
+    }
+
+    protected override void OnBeforeHide()
+    {
+        base.OnBeforeHide();
+        btnRestorePurchase.onClick.RemoveListener(OnClickRestorePurchase);
+        btnShowPrivacyConsent.onClick.RemoveListener(OnClickShowPrivacyConsent);
+    }
+
+    void SetupButtonDefault()
+    {
+        btnRestorePurchase.gameObject.SetActive(false);
+        btnShowPrivacyConsent.gameObject.SetActive(false);
+    }
+
+    public void OnClickRestorePurchase()
+    {
+        restorePurchaseEvent.Raise();
+    }
+
+    public void OnClickShowPrivacyConsent()
+    {
+        callShowAgainGDPREvent.Raise();
+    }
 }
