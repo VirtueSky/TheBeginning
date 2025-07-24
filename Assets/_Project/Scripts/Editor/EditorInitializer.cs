ce26a05f062b68953c8a4eac992dee549c527204
Ducnv <ducnv.cntt0502@gmail.com>
2025-07-24 21:38:58 +0700

    - update EditorInititalizer

--- Assets/_Project/Scripts/Editor/EditorInitializer.cs
+++ Assets/_Project/Scripts/Editor/EditorInitializer.cs
@@ -1,21 +1,44 @@
 #if UNITY_EDITOR
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.SceneManagement;
-using Cysharp.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.SceneManagement;
 
+[InitializeOnLoad]
 public static class EditorInitializer
 {
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    public static async void RuntimeEditorInitialize()
+    static EditorInitializer()
     {
-        string currentScene = SceneManager.GetActiveScene().name;
-        switch (currentScene)
+        EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+    }
+
+    private const string serviceScenePath = "Assets/_Project/Scenes/Service.unity";
+    const string SaveSceneKey = "SaveSceneKey";
+
+    private static void OnPlayModeStateChanged(PlayModeStateChange state)
+    {
+        switch (state)
         {
-            case Constant.SERVICE_SCENE:
-                return;
-            case Constant.GAME_SCENE:
-                await Addressables.LoadSceneAsync(Constant.SERVICE_SCENE);
+            case PlayModeStateChange.ExitingEditMode:
+                var activeScenePath = EditorSceneManager.GetActiveScene().path;
+                if (!activeScenePath.Equals(serviceScenePath))
+                {
+                    EditorPrefs.SetString(SaveSceneKey, activeScenePath);
+                    EditorApplication.isPlaying = false;
+                    EditorApplication.delayCall += () =>
+                    {
+                        EditorSceneManager.OpenScene(serviceScenePath);
+                        EditorApplication.isPlaying = true;
+                    };
+                }
+
+                break;
+            case PlayModeStateChange.EnteredEditMode:
+                if (EditorPrefs.HasKey(SaveSceneKey))
+                {
+                    string previousScenePath = EditorPrefs.GetString(SaveSceneKey);
+                    EditorPrefs.DeleteKey(SaveSceneKey);
+                    EditorApplication.delayCall += () => { EditorSceneManager.OpenScene(previousScenePath); };
+                }
+
                 break;
         }
     }
