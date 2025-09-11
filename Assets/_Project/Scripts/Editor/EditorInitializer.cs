a077097bcdd0f1f089546e0080091e3a20b5a874
Ducnv <virtuesky.0520@gmail.com>
2025-09-11 21:57:53 +0700

    Update EditorInitializer.cs

--- Assets/_Project/Scripts/Editor/EditorInitializer.cs
+++ Assets/_Project/Scripts/Editor/EditorInitializer.cs
@@ -5,23 +5,44 @@ using UnityEditor.SceneManagement;
 [InitializeOnLoad]
 public static class EditorInitializer
 {
+    private const string enableAutoSwitchKey = "EditorInitializer_Enable";
+    private const string menuPath = "The Beginning/Auto Switch Service Scene";
+    private const string serviceScenePath = "Assets/_Project/Scenes/Service.unity";
+    private const string saveSceneKey = "SaveSceneKey";
+
     static EditorInitializer()
     {
         EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
     }
 
-    private const string serviceScenePath = "Assets/_Project/Scenes/Service.unity";
-    const string SaveSceneKey = "SaveSceneKey";
+    [MenuItem(menuPath)]
+    private static void ToggleEnable()
+    {
+        bool enabled = !IsEnabled;
+        EditorPrefs.SetBool(enableAutoSwitchKey, enabled);
+        Menu.SetChecked(menuPath, enabled);
+    }
+
+    [MenuItem(menuPath, true)]
+    private static bool ToggleEnableValidate()
+    {
+        Menu.SetChecked(menuPath, IsEnabled);
+        return true;
+    }
+
+    private static bool IsEnabled => EditorPrefs.GetBool(enableAutoSwitchKey, true);
+
 
     private static void OnPlayModeStateChanged(PlayModeStateChange state)
     {
+        if (!IsEnabled) return;
         switch (state)
         {
             case PlayModeStateChange.ExitingEditMode:
                 var activeScenePath = EditorSceneManager.GetActiveScene().path;
                 if (!activeScenePath.Equals(serviceScenePath))
                 {
-                    EditorPrefs.SetString(SaveSceneKey, activeScenePath);
+                    EditorPrefs.SetString(saveSceneKey, activeScenePath);
                     EditorApplication.isPlaying = false;
                     EditorApplication.delayCall += () =>
                     {
@@ -32,10 +53,10 @@ public static class EditorInitializer
 
                 break;
             case PlayModeStateChange.EnteredEditMode:
-                if (EditorPrefs.HasKey(SaveSceneKey))
+                if (EditorPrefs.HasKey(saveSceneKey))
                 {
-                    string previousScenePath = EditorPrefs.GetString(SaveSceneKey);
-                    EditorPrefs.DeleteKey(SaveSceneKey);
+                    string previousScenePath = EditorPrefs.GetString(saveSceneKey);
+                    EditorPrefs.DeleteKey(saveSceneKey);
                     EditorApplication.delayCall += () => { EditorSceneManager.OpenScene(previousScenePath); };
                 }
 
